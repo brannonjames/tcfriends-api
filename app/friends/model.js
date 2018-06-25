eaa7017aaa0ed23e5b485f59a8bb8b558a8f641c
Jimmy Brannon <user.email>
2018-06-25 10:18:27 -0500

    lable whats going on in friend delete middleware

--- app/friends/model.js
+++ app/friends/model.js
@@ -41,12 +41,17 @@ const friendSchema = new mongoose.Schema({
 
 friendSchema.pre('remove', async function(next){
   try {
+    //Remove all photo references
     await db.Photo.remove({
       friend: this._id
     });
+
+    //Remove from shelter
     let shelter = await db.Shelter.findById(this.shelter);
     await shelter.friends.remove(this.id);
     await shelter.save();
+
+    //Remove from users favorites
     let users = await db.User.find({favorites: this.id});
     for(let user of users){
       user.favorites.remove(this.id);

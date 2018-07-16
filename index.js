cd87a930b6134c3bb3a8950470e4e60725bec953
Jimmy Brannon <user.email>
2018-07-16 15:42:18 -0500

    change default port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3069;
 
 const friendRoutes = require('./app/friends');
 const userRoutes = require('./app/users');
